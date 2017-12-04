a04f9db3c9066a5d6ce7ae5965046e03ef0220c3
Arkadiusz Stankiewicz <sughalith@gmail.com>
2017-12-04 17:26:54 +0100

    added User user

--- src/Dot.Library.Database/Model/BookReservation.cs
+++ src/Dot.Library.Database/Model/BookReservation.cs
@@ -5,7 +5,7 @@ namespace Dot.Library.Database.Model
     public class BookReservation
     {
         public int ID { get; set; }
-        public string user { get; set; }
+        public User user { get; set; }
         public List<Book> reservatedBooks { get; set; }
     }
 }
