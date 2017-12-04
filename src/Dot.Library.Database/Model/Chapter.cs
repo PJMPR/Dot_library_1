1653ae9bf192b7d392213037e463487c7a5184cd
Jan Miętki <jan.mietki@gmail.com>
2017-12-04 16:24:37 +0100

    Dodany model spisu treści książki

---
+++ src/Dot.Library.Database/Model/Chapter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dot.Library.Database.Model
+{
+    public class Chapter
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public string Name { get; set; }
+        public List<Chapter> SubChapters { get; set; }
+    }
+}
