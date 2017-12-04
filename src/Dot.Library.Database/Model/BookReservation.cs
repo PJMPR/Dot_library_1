0d0df471eae83bdbcb4dab48555c31f5038389c0
Arkadiusz Stankiewicz <arkadiuszstankiewicz@MacBook-Pro-test.local>
2017-12-04 16:43:26 +0100

    Model rezerwacji. Do poprawy string u usera na typ User

--- src/Dot.Library.Database/Model/BookReservation.cs
+++ src/Dot.Library.Database/Model/BookReservation.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace Dot.Library.Database.Model
 {
     public class BookReservation
     {
-        public BookReservation()
-        {
-        }
+        public int ID { get; set; }
+        public string user { get; set; }
+        public List<Book> reservatedBooks { get; set; }
     }
 }
