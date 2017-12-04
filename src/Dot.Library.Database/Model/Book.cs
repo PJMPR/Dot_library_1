6d23813ddfd549c7c3b8092f4e4130685ac33e1a
Adam Urbanowicz <aurbanow@pjwstk.edu.pl>
2017-12-04 16:04:46 +0100

    Add database project

---
+++ src/Dot.Library.Database/Model/Book.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dot.Library.Database.Model
+{
+    public class Book
+    {
+    }
+}
--- src/Dot.Library.Web/Startup.cs
+++ src/Dot.Library.Web/Startup.cs
@@ -20,13 +20,11 @@ namespace Dot.Library.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -34,7 +32,7 @@ namespace Dot.Library.Web
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
