1225d6f4c99eda8f67b863f3b854d04101d04e01
Arkadiusz Stankiewicz <sughalith@gmail.com>
2017-12-04 17:10:48 +0100

    parent added

--- src/Dot.Library.Database/Model/Category.cs
+++ src/Dot.Library.Database/Model/Category.cs
@@ -6,9 +6,13 @@ using System.Text;
 namespace Dot.Library.Database.Model
 {
 public class Category
-{
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public List<Category> SubCategories { get; set; }
+    {
+
+       
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<Category> SubCategories { get; set; }
+        public Category parent { get; set; }
+
     }
 }
