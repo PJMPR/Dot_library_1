a38bb2310c6160b2f81b612be8c3ca222d47f108
Filip Machaj <s13033@pjwstk.edu.pl>
2017-12-04 17:13:57 +0100

    użytkownik drugie podejście

--- src/Dot.Library.Database/Model/User.cs
+++ src/Dot.Library.Database/Model/User.cs
@@ -6,23 +6,14 @@ namespace Dot.Library.Database.Model
 {
     class User
     {
-        private int id;
-        private string name;
-        private string login;
-        private string pass;
-        private string surname;
-        private string adress;
-        private string postalCode;
-        private List<int> userRent = new List<int>();
+        private int id { get; }
+        private string name { set; get; }
+        private string login { set; get; }
+        private string pass { set; get; }
+        private string surname { set; get; }
+        private string adress { set; get; }
+        private string postalCode { set; get; }
 
-        public global::System.String Name { get => name; set => name = value; }
-        public global::System.String Login { get => login; set => login = value; }
-        public global::System.String Pass { get => pass; set => pass = value; }
-        public global::System.String Surname { get => surname; set => surname = value; }
-        public global::System.String Adress { get => adress; set => adress = value; }
-        public global::System.String PostalCode { get => postalCode; set => postalCode = value; }
-        public global::System.Int32 Id { get => id;  }
-        public global::System.Int32 Rent { get => rent; set => rent = value; }
-        public List<global::System.Int32> UserRent { get => userRent; set => userRent = value; }
+        public List<Int32> UserRent { get; set; } = new List<int>();
     }
 }
