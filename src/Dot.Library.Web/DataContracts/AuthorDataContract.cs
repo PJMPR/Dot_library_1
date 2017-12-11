9e6287669d78cd99e161ab07ccc04ea043715c2f
DESKTOP-AB31AV0\Miau <michkun22@gmail.com>
2017-12-11 16:55:25 +0100

    AuthorDataContract Refactored

---
+++ src/Dot.Library.Web/DataContracts/AuthorDataContract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Dot.Library.Web.DataContracts
+{
+    [DataContract]
+    public class AuthorDataContract
+    {
+        [DataModel]
+        public int ID { get; set; }
+
+        [DataModel]
+        public string Name { get; set; }
+
+        [DataModel]
+        public string Surname { get; set; }
+
+        [DataModel]
+        public List<AuthorBooksDataContract> publishedBooks { get; set; }
+    }
+}
