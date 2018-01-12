1eda4300ebbd1ba295d73bda43c852b3f87e3b4f
Dominika Malinowska <domin_mal@wp.pl>
2018-01-13 00:25:23 +0100

    UserDataContract added

---
+++ src/Dot.Library.Web/DataContracts/UserDataContract.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Dot.Library.Web.DataContracts
+{
+    [DataContract]
+    public class UserDataContract
+    {
+        [DataMember]
+        public int id { set; get; }
+        [DataMember]
+        public string name { set; get; }
+        [DataMember]
+        public string login { set; get; }
+        [DataMember]
+        public string pass { set; get; }
+        [DataMember]
+        public string surname { set; get; }
+        [DataMember]
+        public string adress { set; get; }
+        [DataMember]
+        public string postalCode { set; get; }
+    }
+}
